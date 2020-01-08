4e74fffe667d8eea5760f1b54802095c575bedc3
Diego Domínguez <d.dominguez.glez1111@gmail.com>
2020-01-08 16:50:09 +0100

    Initial Commit

---
+++ Prueba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prueba
+//
+//  Created by Diego Dominguez on 08/01/2020.
+//  Copyright © 2020 Diego Dominguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
